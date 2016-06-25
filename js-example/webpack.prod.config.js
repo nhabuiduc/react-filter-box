86e0bb57e9713e13129ea2c1e08d41b8aa447986
Nha <hugodichoi@gmail.com>
2016-06-25 10:31:40 +0800

    add build for prod in js-example

---
+++ js-example/webpack.prod.config.js
@@ -0,0 +1,26 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+  devtool: 'source-map',
+  entry: [
+    './src/index'
+  ],
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/static/'
+  },
+  plugins: [
+  ],
+  module: {
+    loaders: [{
+      test: /\.js$/,
+      loaders: ['react-hot', 'babel'],
+      include: path.join(__dirname, 'src')
+    },{
+        test: [/\.less$/,/\.css$/],
+        loader: "style!css!less"
+      }]
+  }
+};
