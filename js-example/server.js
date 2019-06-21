69a349f1865e59c5751e6ec685e41b57bb1b1f9a
Nha Bui <ndbui@thoughtworks.com>
2019-06-21 10:39:08 +0700

    Update exampe with new react-filter-box version

--- js-example/server.js
+++ js-example/server.js
@@ -6,7 +6,7 @@ new WebpackDevServer(webpack(config), {
   publicPath: config.output.publicPath,
   hot: true,
   historyApiFallback: true
-}).listen(3011, 'localhost', function (err, result) {
+}).listen(3000, 'localhost', function (err, result) {
   if (err) {
     return console.log(err);
   }
