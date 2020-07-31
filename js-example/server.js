6419ac6c928c227768b4417d522837a800092842
Nha Bui Duc <hugodichoi@gmail.com>
2020-07-31 18:14:18 +0700

    Update js-example to user latest react-filter-box

--- js-example/server.js
+++ js-example/server.js
@@ -6,7 +6,7 @@ new WebpackDevServer(webpack(config), {
   publicPath: config.output.publicPath,
   hot: true,
   historyApiFallback: true
-}).listen(3000, 'localhost', function (err, result) {
+}).listen(30001, 'localhost', function (err, result) {
   if (err) {
     return console.log(err);
   }
